33d20203dd3f972a14751a8d17bb35044ba597f6
Typik_off <typik.goog@gmail.com>
2023-07-21 01:26:57 +0200

    added modded gitignore

--- Library/PackageCache/com.unity.2d.animation@7.0.9/Runtime/SpriteLib/SpriteLibrary.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d2f3c747dcbdc3256369afbd67d7e84c3f63b2453d0d7b4d9728ab4124bc8780
-size 12776
--- Library/PackageCache/com.unity.2d.animation@7.0.9/Runtime/SpriteLib/SpriteLibraryAsset.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c45874a2a205a22bbb84aadc03250235f15c63ebe1d7f594f0159b9450535ef9
-size 13787
